1b7b53844f9197b604ed01816d8cb19dc52194e9
OlehDe <121160567+OlehDe@users.noreply.github.com>
2025-11-05 23:40:51 +0200

    1

--- Valheimopedia/Valheimopedia/urls.py
+++ Valheimopedia/Valheimopedia/urls.py
@@ -1,12 +1,20 @@
+# Valheimopedia/urls.py (Головний файл проєкту)
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings  # <-- Додайте цей імпорт
+from django.conf.urls.static import static  # <-- Додайте цей імпорт
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('', include('main_app.urls')),  # Тут підключена ваша програма
+    # ... інші шляхи
 ]
 
+# 👇 ЦЕЙ БЛОК ВИПРАВИТЬ 404 ПОМИЛКИ ДЛЯ СТИЛІВ ТА СКРИПТІВ
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Обслуговування статичних файлів (CSS, JS) у режимі розробки
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    # Рекомендовано також додати медіа-файли
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
